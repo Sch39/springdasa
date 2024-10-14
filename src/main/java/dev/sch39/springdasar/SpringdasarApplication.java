ce8a144d26425a58b165d38bc1a66c155c624f68
Sch39 <sukronraezpactoer@gmail.com>
2024-10-14 11:40:35 +0700

    Init

---
+++ src/main/java/dev/sch39/springdasar/SpringdasarApplication.java
@@ -0,0 +1,13 @@
+package dev.sch39.springdasar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdasarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdasarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/sch39/springdasar/SpringdasarApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sch39.springdasar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdasarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
